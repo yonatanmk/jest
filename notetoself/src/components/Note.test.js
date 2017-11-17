9abfd47792f280e2518f2252cc2ff44c5d14d563
yonatanmk <yonatanmk@gmail.com>
2017-11-17 15:43:59 -0500

    set up testing, first Note test

---
+++ notetoself/src/components/Note.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Note from './Note';
+
+const props = { note: { text: 'test_note' } };
+
+describe('Note', () => {
+  let note = mount(<Note {...props} />);
+
+  it('renders the note text', () => {
+    console.log(note.debug())
+    expect(note.find('p').text()).toEqual(props.note.text);
+  });
+});
